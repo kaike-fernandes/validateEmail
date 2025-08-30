d55fd750414d4c5d98ef9e4ca13567ce3975fec4
kaike.fernandes <kaike.fernandes@meuinc.com.br>
2025-08-29 23:26:06 -0300

    ajuste para deploy da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const API = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+
+export default function App() {
+
+  const [status, setStatus] = useState("checando...");
+  const [result, setResult] = useState(null);
+  const [message, setMessage] = useState("checando...");
+
+  useEffect( () => {
+    fetch(`${API}/health`)
+    .then(r => r.json())
+    .then(j => setStatus(`OK (${j.env})`))
+    .catch(err => {
+      console.error(err);
+      setStatus("Falhou")
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch(`${API}/`)
+    .then(r =>  r.json())
+    .then(j => setMessage(`${j.message}`))
+    .catch(err => {
+      console.error(err);
+      setStatus("Falhou")
+    });
+  })
+
+  const pingAnalyze = async () => {
+    const req = await fetch(`${API}/analyze`,{
+      method: "POST",
+      headers: {"Content-Type": "application/json" },
+      body: JSON.stringify({text: "Mensagem de exemplo"})
+    });
+    setResult(await req.json());
+    
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style={{fontFamily:"system-ui",padding:24}}>
+      <h1>Email AI (deploy primeiro)</h1>
+      <p>API: {API}</p>
+      <p>Health: {status}</p>
+      <p>Mensagem de boas vindas: {message}</p>
+      <button onClick={pingAnalyze}>Testar /analyze (mock)</button>
+      {result && (
+        <pre style={{background:"#070707ff",padding:12, marginTop:12}}>
+      {JSON.stringify(result, null, 2)}
+        </pre>
+      )}
+    </div>
   )
 }
-
-export default App
